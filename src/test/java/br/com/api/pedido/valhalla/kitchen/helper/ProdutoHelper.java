bc883980c3a7965d82d10679c526430db645232d
Caio Le Petit <caio.lepetit@gmail.com>
2024-01-29 21:54:16 -0300

    test: Adicionando os testes dos produtos

--- src/test/java/br/com/api/pedido/valhalla/kitchen/helper/ProdutoHelper.java
+++ src/test/java/br/com/api/pedido/valhalla/kitchen/helper/ProdutoHelper.java
@@ -1,5 +1,6 @@
 package br.com.api.pedido.valhalla.kitchen.helper;
 
+import br.com.api.pedido.valhalla.kitchen.adapter.driven.infra.entity.ProdutoEntity;
 import br.com.api.pedido.valhalla.kitchen.adapter.driver.form.ProdutoForm;
 import br.com.api.pedido.valhalla.kitchen.core.domain.Produto;
 
@@ -17,6 +18,14 @@ public class ProdutoHelper {
         return produtos;
     }
 
+    public static List<ProdutoEntity> gerarListaProdutosEntity () throws IOException {
+        List<ProdutoEntity> produtos = new ArrayList<>();
+        produtos.add(ConverterFilesHelper.convertJsonToObject("produto-entity-lanche-um.json", ProdutoEntity.class));
+        produtos.add(ConverterFilesHelper.convertJsonToObject("produto-entity-lanche-dois.json", ProdutoEntity.class));
+
+        return produtos;
+    }
+
     public static Produto gerarProduto() throws IOException {
         return ConverterFilesHelper.convertJsonToObject("produto-lanche-um.json", Produto.class);
     }
@@ -27,6 +36,14 @@ public class ProdutoHelper {
 
     public static Produto gerarProdutoRequest() throws IOException {
         return ConverterFilesHelper.convertJsonToObject("produto-form.json", Produto.class);
+    }
+
+    public static ProdutoEntity gerarProdutoEntity () throws IOException {
+        return ConverterFilesHelper.convertJsonToObject("produto-entity-lanche-um.json", ProdutoEntity.class);
+    }
+
+    public static ProdutoEntity gerarProdutoEntityRequest () throws IOException {
+        return ConverterFilesHelper.convertJsonToObject("produto-entity-request.json", ProdutoEntity.class);
     }
 
 }
